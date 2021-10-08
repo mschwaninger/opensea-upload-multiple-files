5b9993225c2c44933b5f7be40e052f8f9a99fd7b
mschwaninger <matthias.schwaninger@gritec.ch>
2021-10-08 21:49:16 +0200

    Add statistic methode for images

--- opensea-upload-multiple-files.py
+++ opensea-upload-multiple-files.py
@@ -7,8 +7,16 @@ from selenium.webdriver.support import expected_conditions as ExpectedConditions
 import time
 import sys
 import os
+import json
+
+attributlist = []
+attributdict = {} 
 
 def uploadFiles(startItemId, count, mnemonicString, walletPwd):
+    statisticCreator()
+    global attributdict
+    print(attributdict)
+
     extension_path='./10.2.0_0.crx'
     opt = webdriver.ChromeOptions()
     opt.add_extension(extension_path)
@@ -41,6 +49,23 @@ def uploadFiles(startItemId, count, mnemonicString, walletPwd):
     imageUpload.send_keys(imagePath)
 
     while(True):{}
+
+
+def statisticCreator():
+    global attributdict
+    global attributlist
+    with open("./metadata.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    for _data in data: 
+        for _attributes in _data['attributes']: 
+            attributlist.append(_attributes["value"])
+            if _attributes["value"] not in attributdict.keys():
+                attributdict[_attributes["value"]] = 0
+
+    for _attributdict in attributdict.keys(): 
+        attributdict[_attributdict] = 100/len(attributlist) * attributlist.count(_attributdict)
+
 
 def signIntoMeta(driver, wait, mnemonicString, walletPwd): 
     tabs2 = driver.window_handles
